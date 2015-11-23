1b7d5f589cc97a1ca29de6bdf4193d652e1066c4
Liow Chit Chong <ccliow285@hotmail.com>
2015-11-23 12:13:44 +0800

    jbj

---
+++ ConsoleApplication2test/ConsoleApplication2test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hackintosh");
+            Console.ReadLine();
+        }
+    }
+}
